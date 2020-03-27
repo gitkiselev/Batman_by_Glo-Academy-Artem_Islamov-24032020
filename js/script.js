8afbeaf794164bbeb46be0e44910d2869ce7c0a8
gitkiselev <482003@mail.ru>
2020-03-27 09:32:20 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
     let tabsItem = $('.tabs-item');
     tabsItem.on('click', function(e) {
@@ -10,17 +9,5 @@ $(document).ready(function () {
         $('.tabs-item-active').toggleClass('tabs-item-active');
         $(this).toggleClass('tabs-item-active');
     })
-=======
-$(document).ready(function () {
-    let tabsItem = $('.tabs-item');
-    tabsItem.on('click', function(e) {
-        e.preventDefault();
-        let activeContent = $(this).attr('href');
-        console.log(activeContent);
-        $('.visible').toggleClass('visible');
-        $(activeContent).toggleClass('visible');
-        $('.tabs-item-active').toggleClass('tabs-item-active');
-        $(this).toggleClass('tabs-item-active');
-    })
->>>>>>> 5a1c7921c393938f93db21a94ac699b1fdbd88c0
+
 });
